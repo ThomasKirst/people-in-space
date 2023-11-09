fa85a3d17a91463ce812af3a1546be348511f9a4
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-11-09 17:31:24 +0100

    add solution

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Sanity check");
+
+const numberOfPeopleInSpace = document.querySelector(
+  '[data-js="people-in-space"]'
+);
+const peopleList = document.querySelector('[data-js="people-list"]');
+const issButton = document.querySelector("[data-js='iss-button']");
+const tiangongButton = document.querySelector("[data-js='tiangong-button']");
+const allButton = document.querySelector("[data-js='all-button']");
+
+let peopleInSpace = [];
+
+async function getPeopleInSpace() {
+  const response = await fetch("http://api.open-notify.org/astros.json");
+  const data = await response.json();
+
+  peopleInSpace = data.people;
+  numberOfPeopleInSpace.textContent = data.number;
+
+  renderPeopleInSpace(data.people);
+}
+
+getPeopleInSpace();
+
+function renderPeopleInSpace(people) {
+  peopleList.innerHTML = "";
+
+  people.forEach((person) => {
+    const card = PersonCard(person);
+    peopleList.append(card);
+  });
+}
+
+function PersonCard(person) {
+  const listItem = document.createElement("li");
+  listItem.textContent = person.name;
+
+  return listItem;
+}
+
+function Button(name, onClick) {
+  const button = document.createElement("button");
+  button.setAttribute("type", "button");
+  button.textContent = name;
+  button.addEventListener("click", onClick);
+
+  return button;
+}
+
+allButton.addEventListener("click", () => {
+  renderPeopleInSpace(peopleInSpace);
+});
+
+issButton.addEventListener("click", () => {
+  const issPeople = peopleInSpace.filter((person) => person.craft === "ISS");
+  renderPeopleInSpace(issPeople);
+});
+
+tiangongButton.addEventListener("click", () => {
+  const tiangongPeople = peopleInSpace.filter(
+    (person) => person.craft === "Tiangong"
+  );
+  renderPeopleInSpace(tiangongPeople);
+});
